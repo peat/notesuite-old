a877a7f2dd94f3414ab397f41ad54ec08a03aeac
Peat Bakke <peat@peat.org>
2011-05-25 19:53:22 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteSuite::Application.config.secret_token = '195e9b2655695dfab674b258be7815c5b6a2948954d15d59f3091ba70edda8093dddf7878341d61310cc5f62edc0e60faa23b864f28bae5ad6ce63424b1a5e6e'

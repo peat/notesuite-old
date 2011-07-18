e416a5788934179358a41007c9ea10fda448c3f8
Peat Bakke <peat@peat.org>
2011-07-18 10:35:33 -0700

    Adding recent notes, oddities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
 
+  def index
+    @recent_notes = Note.limit(5).order('updated_at DESC')
+    
+    @oddities = []
+    
+    Master.all.each do |m|
+      if m.notes.count == 0
+        @oddities << { :message => "Master with no notes.", :master => m }
+      end
+    end
+  end
+
   def data_entry
     @authority = Authority.new :region => Region.find_by_id( session[:last_country] )
     @region = Region.new

0b8e38dc0c26f003482c42f485ef3f5ea10cef16
Peat Bakke <peat@peat.org>
2011-07-18 11:04:46 -0700

    Adding a bunch of oddities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,33 @@ class PagesController < ApplicationController
     
     @oddities = []
     
-    Master.all.each do |m|
+    # find masters without any notes
+    Master.includes(:notes).all.each do |m|
       if m.notes.count == 0
-        @oddities << { :message => "Master with no notes.", :master => m }
+        @oddities << { :message => "Master with no Notes.", :master => m }
       end
     end
+    
+    # find currencies without any masters
+    Currency.includes(:masters).all.each do |c|
+      if c.masters.count == 0
+        @oddities << { :message => "Currency with no Masters.", :currency => c }
+      end
+    end
+    
+    # find authorities without masters
+    Authority.includes(:masters).all.each do |a|
+      if a.masters.count == 0
+        @oddities << { :message => "Authority with no Masters.", :authority => a }
+      end
+    end
+    
+    Region.includes(:masters).all.each do |r|
+      if r.children.blank? and r.masters.count == 0
+        @oddities << { :message => "Region with no Masters.", :region => r }
+      end
+    end
+    
   end
 
   def data_entry

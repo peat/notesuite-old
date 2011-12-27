4825a57bbbde44b360a9f779299901b3040ee477
Peat Bakke <peat@peat.org>
2011-12-27 04:45:24 -0800

    Freaky SQL ordering to handle mixed Pick codes

--- app/models/master_catalog.rb
+++ app/models/master_catalog.rb
@@ -2,5 +2,5 @@ class MasterCatalog < ActiveRecord::Base
   set_primary_key :master_id
   set_table_name :master_catalog
 
-  default_scope order('currency_region, master_code')
+  default_scope order("currency_region, substring(master_code FROM 'P-[A-Za-z]?(\\d+)')::int")
 end
--- app/models/note_catalog.rb
+++ app/models/note_catalog.rb
@@ -1,4 +1,6 @@
 class NoteCatalog < ActiveRecord::Base
   set_primary_key :note_id
   set_table_name :note_catalog
+
+  default_scope order("currency_region, substring( master_code FROM 'P-[A-Za-z]?(\\d+)')::int")
 end
